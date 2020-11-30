13233dde7d86342049f9740564dcb05816042339
seo-dasom <74850449+seo-dasom@users.noreply.github.com>
2020-11-30 15:41:43 +0900

    test 15-2

---
+++ RemoteControlExample.java
@@ -0,0 +1,17 @@
+package sec07.exam01_enum;
+
+public class RemoteControlExample {
+	public static void main(String[] args) {
+		
+		RemoteControl rc = null;
+		
+		rc = new Television();
+		rc.turnOn();
+		rc.turnOff();
+		
+		rc = new Audio();
+		rc.turnOn();
+		rc.turnOff();
+		
+	}
+}
