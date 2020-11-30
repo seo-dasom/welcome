553ce9009eb254f855e65301f0c78a0ffacabda8
seo-dasom <74850449+seo-dasom@users.noreply.github.com>
2020-11-30 14:09:12 +0900

    test 15

---
+++ Dog.java
@@ -0,0 +1,12 @@
+package sec07.exam01_enum;
+
+public class Dog extends Animal {
+	public Dog() {
+		this.kind = "Æ÷À¯·ù";
+	}
+	
+	@Override
+	public void sound() {
+		System.out.println("¸Û¸Û");
+	}
+}
