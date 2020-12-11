887129a5997e398b780b67d83bd45a3c155b4b5d
seo-dasom <74850449+seo-dasom@users.noreply.github.com>
2020-12-11 21:11:18 +0900

    test 29-2

---
+++ Coin.java
@@ -0,0 +1,13 @@
+package sec07.exam01_enum;
+
+public class Coin {
+	private int value;
+	
+	public Coin(int value) {
+		this.value = value;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+}
---
+++ Fruit.java
@@ -0,0 +1,11 @@
+package sec07.exam01_enum;
+
+public class Fruit {
+	public String name;
+	public int price;
+	
+	public Fruit(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+}
